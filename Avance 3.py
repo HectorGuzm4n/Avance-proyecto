1bb7e62004bebe0d3c1456928336fa0c0adfc93a
HectorGuzm4n <111297327+HectorGuzm4n@users.noreply.github.com>
2022-09-01 11:29:43 -0500

    Update Avance 3.py

--- Avance 3.py
+++ Avance 3.py
@@ -21,10 +21,10 @@ fin
 
 
 a,b,c,d,e,f,g,h= (K= 42354, W= 400),(K=2456, W=454),(K= 554 W= 43),(K= 848 W=165), (K= 68515 W= 545), (K= 846, W=50),(K= 407867 W= 5461), (K= 47 W= 5)
-print ("Nombre de jugador")
+input ("Nombre de jugador")
 kills= K
 wins= W
 score= (K + W)
 print (score)
-lista= sum (a,b,c,d,e,f,g,h)
-print sorted(score)
+lista= sum (score(a),score(b),score(c),score(d),score(e),score(f),score(g),score(h))
+print sorted(lista)
