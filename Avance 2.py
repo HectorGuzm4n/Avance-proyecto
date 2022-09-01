9b1525f4082dedc48594a7b00f9ee80c1c4be209
HectorGuzm4n <111297327+HectorGuzm4n@users.noreply.github.com>
2022-09-01 11:21:14 -0500

    Create Avance 2.py

---
+++ Avance 2.py
@@ -0,0 +1,28 @@
+"""
+Ejemplo del uso de los operadores de suma 
+
+Posicionamiento de jugadores competitivos
+
+Inicio
+
+definir "score" como entero
+
+Leer kills
+
+leer wins
+
+score= kills+wins
+
+Imprimir score
+
+ordenar de > a <
+fin
+"""
+
+
+
+kills= input ("numero de eliminaciones")
+wins= input ("numero de victorias")
+score= (kills +wins)
+print (score)
+
