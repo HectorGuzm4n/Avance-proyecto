2a9e60cddf7391bb1a58d33a1bf2bcd1588fe60a
HectorGuzm4n <111297327+HectorGuzm4n@users.noreply.github.com>
2022-09-01 11:22:13 -0500

    Create Avance 3.py

---
+++ Avance 3.py
@@ -0,0 +1,30 @@
+"""
+Ejemplo del uso de los operadores de suma 
+
+Posicionamiento de jugadores competitivos
+
+Inicio
+
+definir "score" como entero
+
+Leer kills
+
+leer wins
+
+score= kills+wins
+
+Imprimir score
+
+ordenar de > a <
+fin
+"""
+
+
+a,b,c,d,e,f,g,h= (K= 42354, W= 400),(K=2456, W=454),(K= 554 W= 43),(K= 848 W=165), (K= 68515 W= 545), (K= 846, W=50),(K= 407867 W= 5461), (K= 47 W= 5)
+print ("Nombre de jugador")
+kills= K
+wins= W
+score= (K + W)
+print (score)
+lista= sum (a,b,c,d,e,f,g,h)
+print sorted(score)
