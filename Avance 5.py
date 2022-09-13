50021a56d16ea1d6cc82d98f779cdd7ed5744c9d
HectorGuzm4n <111297327+HectorGuzm4n@users.noreply.github.com>
2022-09-13 15:03:40 -0500

    Update Avance 5.py

--- Avance 5.py
+++ Avance 5.py
@@ -1,1 +1,57 @@
+"""
+
+Posicionamiento de jugadores competitivos
+Inicio
+Ingresar nombre de jugafor y contraseña
+mientras constraseña != contraseña
+entonces
+Imprimir ("su contraseña es incorrecta")
+definir "score" como entero
+Leer kills
+leer wins
+score= kills+wins
+Imprimir score
+si score < 8000 entonces
+imprimir ("tus puntos no son suficientes para rankearte")
+SiNO
+ordenar de > a <
+fin
+"""
+print ("¿Ya tiene una cuenta?")
+respuesta= input ("si")
+respuesta= input ("No")
+
+if
+respuesta= ("No")
+jugador= ("ingrese su nombrte de jugador")
+contraseña= ("escriba una contraseña")
+
+else
+respuesta ("Si")
+jugador= input ("ingrese nombre de jugador")
+
+  while
+  jugador!= jugador
+  print ("jugador no encontrado")
+
+contraseña= input ("ingrese la contraseña")
+
+  while
+  contraseña!= contraseña
+  print ("contraseña incorrecta")
+
+kills= float(input("ingresa numero de eliminaciones"))
+wins= float(input ("Ingresa numero de victorias"))
+
+score= kills + wins
+score= score
+
+   if 
+   score < 8000
+   print ("lo sentimos ", jugador , " no cuentas con los puntos suficientes para rankearte")
+    print ("tus puntos: ", score , " puntos para rankerate: 8000")
+
+    else 
+    print (lista_jugadores)
+   print ("felicidades has logrado clasificarte en el ranking con ", score , "puntos")
 
