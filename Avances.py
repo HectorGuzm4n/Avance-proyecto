b154dd817cf415bb13f7f574d6bffcab69ba690c
HectorGuzm4n <111297327+HectorGuzm4n@users.noreply.github.com>
2022-10-15 14:14:16 -0500

    Update Avances.py

--- Avances.py
+++ Avances.py
@@ -63,7 +63,7 @@ else :
   
   stats= {wins, kills}
   
-ranking= [[nombre , score], [stats] ]          //los valores de la lista variaran dependiendo del juego y los puntos de cada jugador con una cuenta vinculada//
+ranking= [[jugador, score], [stats] ]          //los valores de la lista variaran dependiendo del juego y los puntos de cada jugador con una cuenta vinculada//
 ranking.sort ()
 ranking.insert (5,score)
 ranking.sort (decrease)
